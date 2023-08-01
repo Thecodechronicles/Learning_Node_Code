670a7a3e489933c6e8f1b284d1d075e56933ef82
Ankit Mittal <ankitmittal524@gmail.com>
2023-08-01 20:26:43 +0530

    minor changes in Mongo dataset file

--- Mongo_task-manager-api_DataSet.js
+++ Mongo_task-manager-api_DataSet.js
@@ -213,3 +213,146 @@ var tasks =
             "__v": 0
         }
     ]
+
+
+
+// New Data set 
+
+
+
+var usersTwo =
+    [
+        {
+            "_id": {
+                "$oid": "62f14d3c3d429befc7a80c48"
+            },
+            "name": "Bruce Wayne",
+            "email": "brucewayne@batman.com",
+            "password": "robin12345!",
+            "__v": 0
+        },
+
+        {
+            "_id": {
+                "$oid": "62f337e58b187a95f687fa66"
+            },
+            "name": "Peter Parker",
+            "email": "peterparker@spiderman.com",
+            "password": "greengoblin12345!",
+            "__v": 0
+        },
+
+        {
+            "_id": {
+                "$oid": "62fd27d38c5630bf929c8584"
+            },
+            "name": "Peter Parker5",
+            "email": "peterparker5@spiderman.com",
+            "password": "greengoblin5!",
+            "__v": 0
+        },
+
+        {
+            "_id": {
+                "$oid": "6305e57f4873b993c73564db"
+            },
+            "name": "Peter Parker11",
+            "email": "peterparker11@spiderman.com",
+            "password": "greengoblin11!",
+            "__v": 0
+        },
+
+        // {
+        //     "_id": {
+        //         "$oid": "6306541c10a9736f8d2e6f2d"
+        //     },
+        //     "name": "Jon Snow",
+        //     "email": "jonsnow@azarahai.com",
+        //     "password": "$2a$08$yzc98gcovwwbbax.pkcuco7k6wo3hqncykfelw9f/cf5cwh.hzldy",
+        //     "__v": 0
+        // },
+
+        {
+            "_id": {
+                "$oid": "645f9752b19d358807492da2"
+            },
+            "name": "Tony Stark",
+            "email": "tonystark5@azarahai.com",
+            "password": "mandrainwhite12345!",
+            "__v": 0
+        },
+
+        {
+            "_id": {
+                "$oid": "646df82d09e3d16dc5a8f4f4"
+            },
+            "name": "Tom Stark5",
+            "email": "tonystark5@azarahaiOne.com",
+            "password": "mandrainwhite5!",
+            "__v": 0
+        },
+
+        {
+            "_id": {
+                "$oid": "646e15c40fdaa22e7f81db22"
+            },
+            "name": "Tom Loose",
+            "email": "tonystark5@azahem.com",
+            "password": "mandrainwhite!!",
+            "__v": 0
+        },
+
+        {
+            "_id": {
+                "$oid": "647242e94f005ed8bacdfaca"
+            },
+            "name": "Tom Muse",
+            "email": "tonystark5@azarahem5.com",
+            "password": "letitsnow",
+            "__v": 0
+        }
+    ]
+
+
+var updatable =
+    [
+        {
+            "_id": {
+                "$oid": "64c7dc9f3fb211d19a946ee7"
+            },
+            "name": "Tom Muse5",
+            "email": "tonymuse5@azarahem11.com",
+            "password": "mandrainyellow!",
+            "__v": 0
+        },
+
+        {
+            "_id": {
+                "$oid": "64c7dccf3fb211d19a946ee9"
+            },
+            "name": "Tony Muse11",
+            "email": "tommymuse11@azarahem11.com",
+            "password": "mandrainyellow11!",
+            "__v": 0
+        },
+
+        {
+            "_id": {
+                "$oid": "64c7dd0e3fb211d19a946eeb"
+            },
+            "name": "Mony Fuse5",
+            "email": "fonymuse5@azarahem11.com",
+            "password": "mandraingrey5!",
+            "__v": 0
+        },
+
+        {
+            "_id": {
+                "$oid": "64c80350a5f9c7e5cca18cca"
+            },
+            "name": "Mony Fuse11",
+            "email": "fonymuse11@azarahem11.com",
+            "password": "mandrainyellow11!",
+            "__v": 0
+        }
+    ]
